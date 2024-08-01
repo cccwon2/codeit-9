f87926c440751c7bc3a6144a0617018ede7ad431
junjeeong <wn8624@naver.com>
2024-08-01 15:15:07 +0900

    정준영

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import ColorBox from "./ColorBox";
 import "./App.css";
+import { useState } from "react";
 
-
-//상태를 정의하고, 이 상태를 변경하고, 상태에 따라서 렌더링이 잘 되도록. 
-const domInput = document.querySelector('.selectColor')
+//상태를 정의하고, 이 상태를 변경하고, 상태에 따라서 렌더링이 잘 되도
 
 function App() {
+  const [color, setColor] = useState('#ffffff');
+
+  let changeColor = (e) => setColor(e.currentTarget.value);
+
   return (
     <div className="App">
       <h1>무지개색 상자</h1>
       <div>
-        <input className={'selectColor'} />
+        <input onChange={changeColor} />
       </div>
-      <ColorBox color={domInput.value} />
+      <ColorBox color={color} />
     </div>
   );
 }
