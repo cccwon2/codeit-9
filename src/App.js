e38f1269254720d1f2d34e9fbe62b1fe467c2bcc
junjeeong <wn8624@naver.com>
2024-08-01 14:30:30 +0900

    정준영

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import ColorBox from "./ColorBox";
-
 import "./App.css";
+
+
+//상태를 정의하고, 이 상태를 변경하고, 상태에 따라서 렌더링이 잘 되도록. 
+const domInput = document.querySelector('.selectColor')
 
 function App() {
   return (
     <div className="App">
       <h1>무지개색 상자</h1>
       <div>
-        <input />
+        <input className={'selectColor'} />
       </div>
-      <ColorBox />
+      <ColorBox color={domInput.value} />
     </div>
   );
 }
